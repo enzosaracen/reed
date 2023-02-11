1b22c3b93646df3f8e6a76ae2b9c538c59f5168c
enzo <enzo@enzo.run>
2023-02-10 17:24:00 -0800

    update

--- src/reed.c
+++ src/reed.c
@@ -1,6 +1,7 @@
 #include "galois.h"
 
 #define DIV8	0b11101
+#define DIV4    0b11
 
 typedef struct Reed Reed;
 struct Reed {
@@ -13,23 +14,24 @@ void gen(Reed *r)
 {
 	int i, j;
 
-	#define A0 1
+	#define A0 0
+
+	#define GOL(v)	(r->f->gol[(v) % r->f->size])
+	#define LOG(v)	(r->f->log[(v) % r->f->size])
 
 	r->gen[0] = r->f->gol[A0];
 	r->gen[1] = 1;
 	for(i = 0; i < 2*r->t-1; i++) {
-		for(j = i+1; j >= 0; j--) {
+		for(j = i+1; j >= 0; j--)
 			r->gen[j+1] = r->gen[j];
-			r->gen[j] = 0;
-			if(j == i+1)
-				continue;
-			r->gen[j+1] = (r->gen[j+1] +
-				r->f->gol[(r->f->log[r->gen[j+2]] + i+A0+1) % (r->f->size-1)])
-				% (r->f->size-1);
-			if(j == 0)
-				r->gen[j] = r->f->gol[(r->f->log[r->gen[j+1]] + i+A0+1) % (r->f->size-1)];
+		r->gen[0] = 0;
+		for(j = 0; j <= i+1; j++) {
+			printf("%d\n", LOG(r->gen[j+1]) + i+A0+1);
+			r->gen[j] += GOL(LOG(r->gen[j+1]) + i+A0+1);
+			r->gen[j] %= r->f->size;
 		}
-		break;	
+		if(i == 2)
+			break;
 	}
 }
 
@@ -38,13 +40,15 @@ int main(void)
 	int i;
 	Reed r;
 
-	r.f = init(8, DIV8);
+	r.f = init(4, DIV4);
 	r.n = r.f->size-1;
-	r.k = 239;
+	r.k = 11;
 	r.t = (r.n-r.k) / 2;
 	r.gen = calloc(2*r.t + 1, sizeof(int));
 	gen(&r);
 
 	for(i = 2*r.t; i >= 0; i--)
 		printf("%dx^%d%s", r.gen[i], i, (i == 0) ? "\n" : " + ");
+	for(i = 0; i < r.f->size; i++)
+		printf("(%d, %d)\n", i, r.f->gol[i]);
 }
